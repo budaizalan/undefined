5f4bc5d89899e7e047eb8ff74f8216566027e211
mekis <budai.zalan@students.jedlik.eu>
2024-09-21 17:04:25 +0200

    beginning

---
+++ public/app.js
@@ -0,0 +1,17 @@
+// let sections = document.querySelectorAll("section");
+// let navLinks = document.querySelectorAll("header nav a");
+// let navBar = document.querySelector(".navbar")
+// function onClick() {
+//     console.log('h');
+    
+//     sections.forEach((sec) => {
+//         let id = sec.getAttribute("id");
+//             navLinks.forEach((links) => {
+//                 links.classList.remove("active");
+//                 document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+//             });
+//     });
+// };
+
+// navBar.addEventListener("click", onClick)
+// // onClick();
