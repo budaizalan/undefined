230c29a55045ffe3397184364d65e9905f8c3e40
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2024-09-29 20:40:48 +0200

    Starting task2

--- public/app.js
+++ public/app.js
@@ -1,23 +1,33 @@
 "use strict";
+import Tankage from "./Tankage.js";
 const fizetett = document.getElementById("fizetett");
 const inputDateElement = document.getElementById("inputDate");
 const datum = inputDateElement ? new Date(inputDateElement.value) : new Date();
 var slider = document.getElementById("Beza");
 var output = document.getElementById("display");
+const kmMeterStatus = document.getElementById("kilometerAllas");
+let TANKAGES = [];
 if (output && slider) {
     output.innerHTML = slider.value;
     slider.oninput = function () {
         // Zsa();
         if (output) {
             output.innerHTML = this.value;
+            console.log(output);
         }
     };
 }
 if (fizetett) {
-    fizetett.oninput = function () {
+    fizetett.onblur = function () {
         let fizetetts = fizetett.value;
         if (fizetetts.length < 3) {
             fizetett.value = (parseInt(fizetett.value) * 1000).toString();
         }
     };
 }
+function Add_new_tankage(e) {
+    e.preventDefault();
+    TANKAGES.push(new Tankage(slider.value, datum, fizetett.value, kmMeterStatus.value));
+    console.log(TANKAGES);
+}
+document.getElementById("addTankage")?.addEventListener('click', Add_new_tankage);
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
+import Tankage from "./Tankage";
+
 const fizetett = document.getElementById("fizetett") as HTMLInputElement | null;
 const inputDateElement = document.getElementById("inputDate") as HTMLInputElement | null;
 const datum = inputDateElement ? new Date(inputDateElement.value) : new Date();
 var slider = document.getElementById("Beza") as HTMLInputElement | null;
 var output = document.getElementById("display") as HTMLElement | null;
+const kmMeterStatus = document.getElementById("kilometerAllas") as HTMLInputElement |null;
+
+let TANKAGES: Tankage[] = [];
+
 
 if (output && slider) {
     output.innerHTML = slider.value;
@@ -10,13 +16,14 @@ if (output && slider) {
     slider.oninput = function() {
         // Zsa();
         if (output) {
-            output.innerHTML = (this as HTMLInputElement).value;
+            output.innerHTML = (this as HTMLInputElement).value; 
+            console.log(output);                       
         }
     }
 }
 
 if (fizetett) {
-    fizetett.oninput = function() {
+    fizetett.onblur = function() {
         let fizetetts = fizetett.value;
         
         if (fizetetts.length < 3) {
@@ -24,3 +31,11 @@ if (fizetett) {
         }
     }
 }
+
+function Add_new_tankage(e: Event){
+    e.preventDefault();
+    TANKAGES.push(new Tankage(slider!.value, datum, fizetett!.value, kmMeterStatus!.value))
+    console.log(TANKAGES);    
+}
+
+(document.getElementById("addTankage") as HTMLElement | null)?.addEventListener('click', Add_new_tankage);
