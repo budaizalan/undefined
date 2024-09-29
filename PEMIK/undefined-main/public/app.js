67fdce1a5dec4e01aef6cb50801b557d3d062a19
mekis <72376854+budaizalan@users.noreply.github.com>
2024-09-29 19:40:23 +0200

    fhu

---
+++ PEMIK/undefined-main/public/app.js
@@ -0,0 +1,43 @@
+"use strict";
+let fizetett = document.getElementById("fizetett");
+const inputDateElement = document.getElementById("inputDate");
+const datum = inputDateElement ? new Date(inputDateElement.value) : new Date();
+var slider = document.getElementById("Beza");
+var output = document.getElementById("display");
+const hozzaad = document.getElementById("hozzaad");
+hozzaad?.addEventListener("click", Calc);
+function Calc() {
+    let fizetettv;
+    var inputDate = new Date(datum).toISOString().slice(0, 10);
+    console.log(fizetett?.value, inputDate);
+    if (fizetett.value.toString().length < 4) {
+        fizetettv = parseInt(fizetett.value) * 1000;
+    }
+    else {
+        fizetettv = fizetett.value;
+    }
+    const Holder = document.querySelector(".sumRec");
+    var divElement = document.createElement("Div");
+    divElement.className = "tankolasRecord";
+    var text = document.createTextNode(`${inputDate} dátumon: ${slider?.value} litert tankolt ${fizetettv} forintért`);
+    divElement.appendChild(text);
+    Holder?.append(divElement);
+    console.log("done");
+}
+if (output && slider) {
+    output.innerHTML = slider.value;
+    slider.oninput = function () {
+        console.log("fasz");
+        if (output) {
+            output.innerHTML = this.value;
+        }
+    };
+}
+// if (fizetett) {
+//     fizetett.oninput = function() {
+//         let fizetetts = fizetett.value;
+//         if (fizetetts.length < 3) {
+//             fizetett.value = (parseInt(fizetett.value) * 1000).toString();
+//         }
+//     }
+// }
