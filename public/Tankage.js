afe0c3d639dd2842c351bdf4c7e84c0db2415c9e
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2024-09-30 17:36:22 +0200

    Task 2 v0.7.0

--- public/Tankage.js
+++ public/Tankage.js
@@ -3,9 +3,9 @@ export default class Tankage {
     constructor(_fuel_amount, _date, _cost, _km_meter_status) {
         this.fuel_amount = +_fuel_amount;
         this.date_full = _date;
-        this.day = this.date_full.getDate();
-        this.month = this.date_full.getMonth();
-        this.year = this.date_full.getFullYear();
+        this.day = new Date(this.date_full).getDate();
+        this.month = new Date(this.date_full).getMonth();
+        this.year = new Date(this.date_full).getFullYear();
         this.cost = +_cost;
         this.km_meter_status = +_km_meter_status;
     }
--- public/app.js
+++ public/app.js
@@ -1,33 +1,72 @@
 "use strict";
 import Tankage from "./Tankage.js";
-const fizetett = document.getElementById("fizetett");
+const cost = document.getElementById("fizetett");
 const inputDateElement = document.getElementById("inputDate");
-const datum = inputDateElement ? new Date(inputDateElement.value) : new Date();
+// const date = inputDateElement ? new Date(inputDateElement.value) : new Date();
 var slider = document.getElementById("Beza");
 var output = document.getElementById("display");
 const kmMeterStatus = document.getElementById("kilometerAllas");
+const selectMonths = document.querySelector('#selectMonths');
 let TANKAGES = [];
 if (output && slider) {
     output.innerHTML = slider.value;
     slider.oninput = function () {
-        console.log("fasz");
+        // Zsa();
         if (output) {
             output.innerHTML = this.value;
             console.log(output);
         }
     };
 }
-if (fizetett) {
-    fizetett.onblur = function () {
-        let fizetetts = fizetett.value;
+if (cost) {
+    cost.onblur = function () {
+        let fizetetts = cost.value;
         if (fizetetts.length < 3) {
-            fizetett.value = (parseInt(fizetett.value) * 1000).toString();
+            cost.value = (parseInt(cost.value) * 1000).toString();
         }
     };
 }
 function Add_new_tankage(e) {
     e.preventDefault();
-    TANKAGES.push(new Tankage(slider.value, datum, fizetett.value, kmMeterStatus.value));
+    TANKAGES.push(new Tankage(slider.value, inputDateElement?.valueAsDate, cost.value, kmMeterStatus.value));
+    console.log(inputDateElement?.valueAsDate);
     console.log(TANKAGES);
+    filterTankages();
 }
+function generateMonthsForSelect() {
+    const months_hun = ["Nincs szűrés", "Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
+    for (let i = 0; i < 13; i++) {
+        let option = document.createElement('option');
+        option.value = `${i - 1}`;
+        option.innerHTML = `${months_hun[i]}`;
+        selectMonths?.append(option);
+    }
+}
+function filterTankages() {
+    let filteredTankages = TANKAGES.filter(t => t.month.toString().includes(selectMonths?.value != "-1" ? selectMonths?.value : ""));
+    generateTableContent(filteredTankages);
+}
+function generateTableContent(tankages) {
+    let table = document.querySelector('tbody');
+    table.innerHTML = "";
+    tankages.forEach(t => {
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${t.fuel_amount}</td>
+            <td>${getFormattedDate(t.date_full)}</td>
+            <td>${t.cost}</td>
+        `;
+        table.append(tr);
+    });
+}
+function getFormattedDate(date) {
+    let year = date.getFullYear();
+    let month = (1 + date.getMonth()).toString().padStart(2, '0');
+    let day = date.getDate().toString().padStart(2, '0');
+    return month + '/' + day + '/' + year;
+}
+window.onload = () => {
+    generateMonthsForSelect();
+};
 document.getElementById("addTankage")?.addEventListener('click', Add_new_tankage);
+selectMonths?.addEventListener('change', filterTankages);
