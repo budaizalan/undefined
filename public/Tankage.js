7e2255c7589b8c15715cd0da2f0e005e9580d7c9
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2024-10-01 18:14:28 +0200

    Task2 v9.0.3

--- public/Tankage.js
+++ public/Tankage.js
@@ -3,9 +3,6 @@ export default class Tankage {
     constructor(_fuel_amount, _date, _cost, _km_meter_status) {
         this.fuel_amount = +_fuel_amount;
         this.date_full = _date;
-        this.day = new Date(this.date_full).getDate();
-        this.month = new Date(this.date_full).getMonth();
-        this.year = new Date(this.date_full).getFullYear();
         this.cost = +_cost;
         this.km_meter_status = +_km_meter_status;
     }
